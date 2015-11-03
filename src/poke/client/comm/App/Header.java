9687998f42eb3a3340d40adbe3220b86ec2f05d3
Jon Guan <jonathan.guan@capitalone.com>
2015-11-02 18:12:29 -0800

    first commit

---
+++ src/poke/client/comm/App/Header.java
@@ -0,0 +1,7 @@
+package poke.client.comm.App;
+
+/**
+ * Created by jsx039 on 11/2/15.
+ */
+public class Header {
+}
---
+++ src/poke/client/comm/App/Ping.java
@@ -0,0 +1,19 @@
+package poke.client.comm.App;
+
+/**
+ * Created by jsx039 on 11/2/15.
+ */
+public class Ping {
+    private String tag;
+    private Integer num;
+    
+    public class Builder {
+        private String tag;
+        private Integer num;
+    }
+
+    public Builder newBuilder(){
+
+    }
+
+}
